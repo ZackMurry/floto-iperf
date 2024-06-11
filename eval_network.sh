82886744d2e105b326957393d432c3f119ced55f
Zack Murry <zackk.murryy@gmail.com>
2024-06-11 15:24:41 -0500

    Add iperf call

--- eval_network.sh
+++ eval_network.sh
@@ -5,7 +5,9 @@ ifconfig
 echo "Pinging with eth0..."
 ping google.com -c 5 -I eth0
 
-echo "Pinging with wlan0..."
-ping google.com -c 5 -I wlan0
+# echo "Pinging with wlan0..."
+# ping google.com -c 5 -I wlan0
+
+iperf3 -c $IPERF_IP
 
 echo "Done!"
