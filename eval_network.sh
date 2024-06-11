207034784541829f152681f422d6e37e7c8aa292
Zack Murry <zackk.murryy@gmail.com>
2024-06-11 15:28:31 -0500

    Remove ping

--- eval_network.sh
+++ eval_network.sh
@@ -2,8 +2,8 @@ echo "Hello, world!"
 
 ifconfig
 
-echo "Pinging with eth0..."
-ping google.com -c 5 -I eth0
+# echo "Pinging with eth0..."
+# ping google.com -c 5 -I eth0
 
 # echo "Pinging with wlan0..."
 # ping google.com -c 5 -I wlan0
